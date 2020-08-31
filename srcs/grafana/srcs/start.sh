457e02a4197d6934d80d24f89fcb95bdfd19cfae
Franciszer <ftthierree@gmail.com>
2020-08-31 17:10:34 +0200

    oyeah

--- srcs/grafana/srcs/start.sh
+++
@@ -1,3 +0,0 @@
-rm -f /grafana-7.0.3/conf/provisioning/dashboards/*.json
-cd ./grafana-7.0.3/bin/
-./grafana-server
--- srcs/nginx/srcs/setup.sh
+++
@@ -1,5 +0,0 @@
-adduser -D "admin"
-echo "admin:admin" | chpasswd
-
-/usr/sbin/sshd
-nginx -g "daemon off;"
