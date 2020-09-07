50cb9f91c5a38ee04f0c9a8dd5afa6100527870e
Franciszer <ftthierree@gmail.com>
2020-09-07 15:41:22 +0200

    oever

--- srcs/nginx/srcs/ssh_check.sh
+++
@@ -1,6 +0,0 @@
-#check if nginx and sshd are running
-while true;do
-        ssh_running=$(pgrep sshd:)
-	[[ "$(echo "$ssh_running")" == '' ]]
-	sleep 5s
-done
--- srcs/nginx/srcs/start.sh
+++
@@ -1,6 +0,0 @@
-adduser -D "admin"
-echo "admin:admin" | chpasswd
-
-# /etc/init.d/ssh_check.sh &
-telegraf &
-nginx -g 'daemon off;'
